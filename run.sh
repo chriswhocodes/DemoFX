ece8f521eb79987be8eee8c49a09bf5efb8d9461
Chris Newland <chris@chrisnewland.com>
2016-11-23 11:07:52 +0000

    set exec permission on gradlew, wrapped java path in quotes to fix on Windows

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export CLASSPATH=target/DemoFX.jar
 export CLASSPATH=$CLASSPATH:src/main/resources
 
 # May need to set -Xmx1024m on low RAM  machines
-$JAVA_HOME/bin/java -cp $CLASSPATH com.chrisnewland.demofx.DemoFXApplication $@
+"$JAVA_HOME/bin/java" -cp $CLASSPATH com.chrisnewland.demofx.DemoFXApplication $@
