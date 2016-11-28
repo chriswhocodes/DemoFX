3a36b612f192c417e4f45025079edb2fdd6c34cc
ciruman <dicir00@gmail.com>
2016-11-29 00:20:42 +0100

    - Removed mobile implementation - Configure basic gradle project - Remove ant

--- softRun.sh
+++ softRun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CLASSPATH=target/DemoFX.jar
+export CLASSPATH=build/libs/DemoFX.jar
 CLASSPATH=$CLASSPATH:$JAVA_HOME/jre/lib/jfxrt.jar
 
 java -Dprism.order=sw -cp $CLASSPATH com.chrisnewland.demofx.DemoFXApplication $@
